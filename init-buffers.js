5d498f7b623bbbe655499c4399ed08900326b49b
AlmaCRH <alma5crh@gmail.com>
2025-04-15 14:19:10 +0100

    test

--- init-buffers.js
+++ init-buffers.js
@@ -15,7 +15,24 @@ const initPositionBuffer = (gl) => {
 
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
 
-  return positionBuffer;
+  const colorBuffer = initColorBuffer(gl);
+
+  return {
+    position: positionBuffer,
+    color: colorBuffer,
+  };
+};
+
+const initColorBuffer = (gl) => {
+  const colors = [
+    1.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.0, 0.0, 1.0,
+    1.0,
+  ];
+
+  const colorBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
+  return colorBuffer;
 };
 
 export { initBuffers };
--- webgl-demo.js
+++ webgl-demo.js
@@ -16,10 +16,13 @@ const main = () => {
 
   const vsSource = `
     attribute vec4 aVertexPosition;
+    attribute vec4 aVertexColor;
     uniform mat4 uModelViewMatrix;
     uniform mat4 uProjectionMatrix;
+    varying lowp vec4 vColor;
     void main() {
     gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
+    vColor = aVertexColor;
     }
   `;
 
